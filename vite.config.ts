87868cb1b0daa61f693129f2d3e8305989d5794b
naresh33-r <nareshmarquez448@gmail.com>
2026-02-10 08:47:13 +0530

    modify the files

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: "/portfilo-web-app/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
